557f3799dfb0cbc715019b5bf72d0c40a11fd917
marsmorse <marsmorse@gmail.com>
2021-03-30 13:15:33 -0700

    [UPDATE] userCreative Model with getAll creatives with a userID. [UPDATE] event model to query songkick API

--- Server/models/event.js
+++ Server/models/event.js
@@ -1,9 +1,22 @@
 var pool = require('../db.js')
-
-class CreativeModel {
-
+//const userC
+var axiosInstance = require('../axios')
+class EventModel {
     getAll() {
-
+        return new Promise((resolve, reject) => {
+            instance.get(`/artists/${a_id}/calendar.json?apikey=${config.songkickAPIkey}`)
+            .then(function (response) {
+                // handle success
+                let res = []
+                console.log(response.data.resultsPage.results.artist);
+                console.log(response.data.resultsPage.results.artist[0].id);
+                req.body.songkickArtistID = response.data.resultsPage.results.artist[0].id;
+                req.body.name = response.data.resultsPage.results.artist[0].displayName;
+                resolve(res);
+            }) 
+            .catch(err => {
+                reject(err);
+            }) 
+        });
     }
-
 }
