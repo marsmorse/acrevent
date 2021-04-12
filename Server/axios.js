2aeac715a23135ea45785b80176adf1a413cc5a4
marsmorse <marsmorse@gmail.com>
2021-04-12 12:16:10 -0700

    [UPDATE] deleted files old location befor emove to src. [DELETE] react logo in Client directory

--- Server/axios.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const instance = axios.create({
-    baseURL: 'https://api.songkick.com/api/3.0/',
-    timeout: 2000,
-    headers: {'X-Custom-Header': 'foobar'}
-  });
-
-  module.exports = instance;
